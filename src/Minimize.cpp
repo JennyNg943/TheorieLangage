9e3f47572b7ad6fb01321db93016dbdc9962ee34
Unknown <tlabrux@gmail.com>
2017-12-19 17:32:21 +0100

    Minimize Vers 2

--- src/Minimize.cpp
+++ src/Minimize.cpp
@@ -14,7 +14,9 @@ sAutoNDE Minimize(const sAutoNDE& at){
   //TODO définir cette fonction
 
   sAutoNDE r;
+  vector<vector<etatset_t>> trans;
   r = Determinize(at);
+  set<int> classes;
   vector<int> tab(r.trans.size(),1);
   for(size_t i = 0 ; i != r.trans.size() ; ++i ){
     for(etatset_t::iterator it_et = r.finaux.begin() ; it_et != r.finaux.end() ; it_et++ ){
@@ -35,26 +37,61 @@ sAutoNDE Minimize(const sAutoNDE& at){
           }
         }
         if(tab[j] == 0){
-          if(tab[i] == 0 && memeEtat == 1){
+          if(tab[i] == 0){
+            if(memeEtat == 1){
               tab[j] = nbClasse +1;
               nbClasse++;
+            }
           }
         }else if(tab[i] == tab[j]){
-          if(memeEtat == 0){
+            //cout <<"Test" << j <<" "<< tab[j] << endl;
+          if(memeEtat == 0 && tab[i] != 0){
             tab[j] = tab[i];
-            cout << "e1 " << j <<" "<< tab[j] << endl;
+            //cout << "e1 " << j <<" "<< tab[j] << endl;
           }else{
             tab[j] = nbClasse +1;
             nbClasse++;
-            cout << "e2 " << j <<" "<< tab[j] << endl;
+            //cout << "e2 " << j <<" "<< tab[j] << endl;
           }
         }
 
       }
-      cout << i << " "<< tab[i] << endl;
+      //cout << i << " "<< tab[i] << endl;
     }
 
+  for(size_t i =0; i < tab.size(); i++){
+    classes.insert(tab[i]);
+  }
+  
+  vector<etatset_t> new_etats(classes.size());
+  for(size_t i =0; i < tab.size(); i++){
+    new_etats[tab[i]].insert((etat_t) i);
+  }
+  
+  trans.resize(new_etats.size());
+  for(size_t i = 0; i < new_etats.size(); i++){
+    trans[i].resize(at.nb_symbs);
+  }
 
+
+    for(size_t in = 0; in < new_etats.size(); in++){
+      for(set<etat_t>::iterator ite = new_etats[in].begin() ; ite != new_etats[in].end() ; ite++){
+        for(size_t c = 0 ; c < at.nb_symbs; c++){
+          for(size_t i = 0 ; i < new_etats.size(); i++){
+            for(set<etat_t>::iterator it = r.trans[*ite][c].begin() ; it != r.trans[*ite][c].end() ; it++){
+              if(new_etats[i].find(*it) != new_etats[i].end()){
+                trans[in][c].insert((etat_t)i);
+                cout << r.initial << " " << in << " " << *ite << endl;
+              }
+            }
+          }
+        }
+      }
+    }
+    
+  r.initial = tab[r.initial];
+
+r.trans = trans;
   return r;
 }
 
